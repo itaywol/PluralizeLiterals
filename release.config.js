5b806f4ce8d96a6f4afca9db0dbe652a2832d732
Itay Wolfish <wo.itay@gmail.com>
2022-09-20 15:03:38 +0300

    fix(semantic release): fix target branch

--- release.config.js
+++ release.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  branches: ["master"],
+  branches: ["main"],
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
@@ -14,7 +14,12 @@ module.exports = {
     [
       "@semantic-release/git",
       {
-        assets: ["lib/**/*.{js|d.ts}", ".github/docs/*", "package.json", "README.md"],
+        assets: [
+          "lib/**/*.{js|d.ts}",
+          ".github/docs/*",
+          "package.json",
+          "README.md",
+        ],
         message:
           "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
       },
