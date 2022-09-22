a87375474a0deb1b53e03291fef5a177f8514c34
Itay Wolfish <wo.itay@gmail.com>
2022-09-22 16:14:47 +0300

    test(pluralize tests): add more tests

--- test/test.ts
+++ test/test.ts
@@ -5,3 +5,4 @@ const wolves: Pluralize<"wolf"> = "wolves";
 const volcanoes: Pluralize<"volacano"> = "volacanoes";
 const sheep: Pluralize<"sheep"> = "sheep";
 const fezzes: Pluralize<"fez"> = "fezzes";
+const repositories: Pluralize<"repository"> = "repositories";
